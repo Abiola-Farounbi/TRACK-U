8ed1d4f659fcfbfe53cc345dc1f384e08da98841
Abiola <49586787+Abiola-Farounbi@users.noreply.github.com>
2022-01-09 21:09:47 +0100

    Fixed bug related to map

--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -35,28 +35,27 @@ export default {
     },
   },
   methods: {
-  
-      displayMap(){
-      // import the required JavaScript file for displaying the map
+    displayMap(){
+       // import the required JavaScript file for displaying the map
       let mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
-      //access token
+       //access token
       mapboxgl.accessToken =  process.env.VUE_APP_MAP_TOKEN
-      // the map object
       const map = new mapboxgl.Map({
       container: 'mapContainer', // container ID
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [this.longitude, this.latitude],
-      zoom: 10
+      style: "mapbox://styles/mapbox/streets-v11",// style URL
+      center: [this.longitude, this.latitude], // starting position [lng, lat]
+      zoom: 5 // starting zoom    
       });
-    //adding zoom and rotation controls
+      //adding zoom and rotation controls
      map.addControl(new mapboxgl.NavigationControl());
 
      
-    //  Create a default marker colored black.
+    //  Create a default Marker, colored black.
     const marker = new mapboxgl.Marker({ color: 'black' })
     .setLngLat([this.longitude, this.latitude])
     marker.addTo(map);
   },
+      
     }
 }
 </script>
